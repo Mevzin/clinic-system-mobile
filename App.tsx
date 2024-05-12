70b41cc264e9df35ba2855016e79d13bb7096a9d
Thiago Torres de Souzam <thiagomev@gmail.com>
2024-05-12 11:00:41 -0300

    Add nativecn and nativewind

--- App.tsx
+++ App.tsx
@@ -1,17 +1,12 @@
-import { ThemeProvider } from 'styled-components';
 import Login from './src/screens/Login';
-import theme from './src/utils/theme';
-import { Container } from './style';
-import { StatusBar } from 'react-native';
+import { StatusBar, View } from 'react-native';
 import "./src/utils/global.css"
 
 export default function App() {
   return (
-    <ThemeProvider theme={theme}>
+    <>
       <StatusBar barStyle={'default'}/>
-      <Container>
-        <Login/>
-      </Container>
-    </ThemeProvider>
+      <Login/>
+    </>
   );
 }
--- src/screens/Login/index.tsx
+++ src/screens/Login/index.tsx
@@ -1,8 +1,10 @@
-import { Alert, Button, Text, TextInput } from "react-native";
-import { Container, Title } from "./styles";
+import { Alert, Text, TextInput, View } from "react-native";
 import { Controller, useForm } from "react-hook-form";
 import { zodResolver } from '@hookform/resolvers/zod';
 import { z } from "zod";
+import { SignInUserFormData } from "../../utils/types";
+import { signInUserFormSchema } from "../../utils/schemas";
+import { Button } from "../../components/Button";
 
 const formSchema = z.object({
     email: z.string().email('Please enter a valid email'),
@@ -11,26 +13,26 @@ const formSchema = z.object({
     });
 
 export default function Login() {
-    const { control, handleSubmit } = useForm({
-        defaultValues: {
+    const form = useForm<SignInUserFormData>({
+      resolver: zodResolver(signInUserFormSchema),
+      defaultValues: {
         email: '',
-        full_name: '',
         password: '',
-        },
-        resolver: zodResolver(formSchema),
-        });
+      }
+    })
       
-    const onSubmit = (data)=>{
-        Alert.alert("Successful", JSON.stringify(data))
-    }
+        function handleSubmit() {}
+
     return(
-        <Container >
-          <Text className="text-white ">Simple Login Form</Text>
+        <View className="flex-1 justify-center items-center bg-slate-900">
+          <Text className="text-white">Login</Text>
   
-          <Button
-            title='Submit'
-            onPress={handleSubmit(onSubmit)}
+          <Button 
+            label="SignIn"
+            className="h-[3rem] w-[6rem] bg-blue-500 "
           />
-        </Container>
+            
+
+        </View>
     )
 }
