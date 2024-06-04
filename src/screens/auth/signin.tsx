e2eb3b89bee06d9bdfabd7f3b9c19f219b7e2b39
Thiago Torres de Souzam <thiagomev@gmail.com>
2024-06-04 02:32:19 -0300

    Change login and cadastrar button

--- src/screens/auth/signin.tsx
+++ src/screens/auth/signin.tsx
@@ -3,67 +3,67 @@ import { useForm } from "react-hook-form";
 import { Button } from "../../components/Button";
 import { zodResolver } from '@hookform/resolvers/zod';
 import { useNavigation } from "@react-navigation/native";
-import { Text,  View, ToastAndroid } from "react-native";
-import  FormInput  from "../../components/FormController";
+import { Text, View, ToastAndroid } from "react-native";
+import FormInput from "../../components/FormController";
 import { SignInUserFormData } from "../../utils/schemas/types";
 import { signInUserFormSchema } from "../../utils/schemas/schemas";
 import { FIREBASE_ERROR } from "../../utils/errors/firebase_errors";
 
 export default function SignIn() {
-  
-    const { signIn } = useAuth()
-    const navigation = useNavigation();
-    const {control, handleSubmit} = useForm<SignInUserFormData>({
-      resolver: zodResolver(signInUserFormSchema),
-      defaultValues: {
-        email: '',
-        password: '',
+
+  const { signIn } = useAuth()
+  const navigation = useNavigation();
+  const { control, handleSubmit } = useForm<SignInUserFormData>({
+    resolver: zodResolver(signInUserFormSchema),
+    defaultValues: {
+      email: '',
+      password: '',
+    }
+  })
+
+  async function signInUser(data: SignInUserFormData) {
+    try {
+      await signIn(data)
+    } catch (error) {
+      if ((error as Error).message === FIREBASE_ERROR.INVALID_CREDENTIAL) {
+        ToastAndroid.show("Email ou senha invalidos!", 5000)
       }
-    })
+    }
+  }
 
-    async function signInUser(data: SignInUserFormData) {
-      try {
-        await signIn(data)
-      } catch (error) {
-        if((error as Error).message === FIREBASE_ERROR.INVALID_CREDENTIAL){
-          ToastAndroid.show("Email ou senha invalidos!", 5000)
-        }
-      }
-    } 
+  return (
+    <View className="flex-1 justify-center items-center bg-slate-900">
+      <Text className="text-white font-bold text-2xl">Login</Text>
+      <FormInput
+        control={control}
+        name={'email'}
+        label="Email"
+        className="w-[20rem] my-3"
+        autoCapitalize={false}
+      />
+      <FormInput
+        control={control}
+        name={'password'}
+        className="w-[20rem] my-3"
+        label="Senha"
+        secureTextEntry={true}
+        autoCapitalize={false}
+      />
+      <Button
+        label="Cadastro ->"
+        labelClasses="text-white"
+        className="mx-1 justify-end"
+        size='default'
+        onPress={() => navigation.navigate('SignUp')}
+      />
 
-    return(
-        <View className="flex-1 justify-center items-center bg-slate-900">
-            <Button 
-              label="Cadastro ->"
-              labelClasses="text-white"
-              className="mt-3 top-1 left-2 absolute"
-              size='default'
-              onPress={() => navigation.navigate('SignUp')}
-            />
-          <Text className="text-white font-bold text-2xl">Login</Text>
-          <FormInput
-            control={control}
-            name={'email'}
-            label="Email"
-            className="w-[20rem] my-3"
-            autoCapitalize={false}
-          />
-          <FormInput
-            control={control}
-            name={'password'}
-            className="w-[20rem] my-3"
-            label="Senha"
-            secureTextEntry={true}
-            autoCapitalize={false}
-          />
-
-          <Button 
-            label="Sign In"
-            labelClasses="text-white"
-            className="bg-cyan-800 mt-3"
-            size='lg'
-            onPress={handleSubmit(signInUser)}
-          />
-        </View>
-    )
+      <Button
+        label="Sign In"
+        labelClasses="text-white"
+        className="bg-cyan-800 mt-3"
+        size='lg'
+        onPress={handleSubmit(signInUser)}
+      />
+    </View>
+  )
 }
--- src/screens/auth/signup.tsx
+++ src/screens/auth/signup.tsx
@@ -1,85 +1,86 @@
-import { Text,  View, ToastAndroid } from "react-native";
+import { Text, View, ToastAndroid } from "react-native";
 import { useForm } from "react-hook-form";
 import { zodResolver } from '@hookform/resolvers/zod';
 import { SignInUserFormData, SignUpUserFormData } from "../../utils/schemas/types";
 import { signUpUserFormSchema } from "../../utils/schemas/schemas";
 import { Button } from "../../components/Button";
-import  FormInput  from "../../components/FormController";
+import FormInput from "../../components/FormController";
 import { useAuth } from "../../hook/Auth";
 import { FIREBASE_ERROR } from "../../utils/errors/firebase_errors";
 import { useNavigation } from "@react-navigation/native";
 
 export default function SignUp() {
-  
-    const { signUp } = useAuth()
-    const navigation = useNavigation();
-    const {control, handleSubmit} = useForm<SignUpUserFormData>({
-        resolver: zodResolver(signUpUserFormSchema),
-        defaultValues: {
-            name: '',
-            email: '',
-            password: '',
-            confirmPassword: '',
-        }
-    })
 
-    async function signUpUser(data: SignUpUserFormData) {
-      try {
-        await signUp(data)
-      } catch (error) {
-        if((error as Error).message === FIREBASE_ERROR.EMAIL_EXISTIS){
-          ToastAndroid.show("Não foi possivel efetuar o cadastro!", 5000)
-        }
+  const { signUp } = useAuth()
+  const navigation = useNavigation();
+  const { control, handleSubmit } = useForm<SignUpUserFormData>({
+    resolver: zodResolver(signUpUserFormSchema),
+    defaultValues: {
+      name: '',
+      email: '',
+      password: '',
+      confirmPassword: '',
+    }
+  })
+
+  async function signUpUser(data: SignUpUserFormData) {
+    try {
+      await signUp(data)
+    } catch (error) {
+      if ((error as Error).message === FIREBASE_ERROR.EMAIL_EXISTIS) {
+        ToastAndroid.show("Não foi possivel efetuar o cadastro!", 5000)
       }
-    } 
+    }
+  }
 
-    return(
-        <View className="flex-1 justify-center items-center bg-slate-900">
-            <Button 
-                label="Login ->"
-                labelClasses="text-white"
-                className="mt-3 top-1 left-2 absolute"
-                size='default'
-                onPress={() => navigation.navigate('SignIn')}
-            />
-          <Text className="text-white font-bold text-2xl">Cadastrar-se</Text>
-          <FormInput
-            control={control}
-            name={'name'}
-            label="Nome"
-            className="w-[20rem] my-3"
-            autoCapitalize={false}
-          />
-          <FormInput
-            control={control}
-            name={'email'}
-            label="Email"
-            className="w-[20rem] my-3"
-            autoCapitalize={false}
-          />
-          <FormInput
-            control={control}
-            name={'password'}
-            className="w-[20rem] my-3"
-            label="Senha"
-            secureTextEntry={true}
-            autoCapitalize={false}
-          />
-         <FormInput
-            control={control}
-            name={'confirmPassword'}
-            className="w-[20rem] my-3"
-            label="Senha"
-            secureTextEntry={true}
-            autoCapitalize={false}
-        />
-          <Button 
-            label="Cadastrar"
-            labelClasses="text-white"
-            className="bg-cyan-800 mt-3"
-            size='lg'
-            onPress={handleSubmit(signUpUser)}
-          />
-        </View>
-    )
+  return (
+    <View className="flex-1 justify-center items-center bg-slate-900">
+
+      <Text className="text-white font-bold text-2xl">Cadastrar-se</Text>
+      <FormInput
+        control={control}
+        name={'name'}
+        label="Nome"
+        className="w-[20rem] my-3"
+        autoCapitalize={false}
+      />
+      <FormInput
+        control={control}
+        name={'email'}
+        label="Email"
+        className="w-[20rem] my-3"
+        autoCapitalize={false}
+      />
+      <FormInput
+        control={control}
+        name={'password'}
+        className="w-[20rem] my-3"
+        label="Senha"
+        secureTextEntry={true}
+        autoCapitalize={false}
+      />
+      <FormInput
+        control={control}
+        name={'confirmPassword'}
+        className="w-[20rem] my-3"
+        label="Senha"
+        secureTextEntry={true}
+        autoCapitalize={false}
+      />
+      <Button
+        label="<- Login"
+        labelClasses="text-white"
+        className="mx-1"
+        size='default'
+        onPress={() => navigation.navigate('SignIn')}
+      />
+      <Button
+        label="Cadastrar"
+        labelClasses="text-white"
+        className="bg-cyan-800 mt-3"
+        size='lg'
+        onPress={handleSubmit(signUpUser)}
+      />
+    </View>
+  )
 }
