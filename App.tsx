d522a20bb9ca8ef47a23f19003e81bd1dabfafac
Thiago Torres de Souzam <thiagomev@gmail.com>
2024-06-07 10:51:59 -0300

    Add changes

--- App.tsx
+++ App.tsx
@@ -4,14 +4,17 @@ import { firebaseConfig } from './src/config/firebase.config';
 import { AuthProvider } from './src/hook/Auth';
 import { Routes } from './src/routes';
 import "./src/style/global.css"
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
 
 initializeApp(firebaseConfig)
 
 export default function App() {
   return (
-    <AuthProvider>
-      <StatusBar barStyle={'light-content'} backgroundColor={"#0f172a"} />
-      <Routes />
-    </AuthProvider>
+    <GestureHandlerRootView>
+      <AuthProvider>
+        <StatusBar barStyle={'light-content'} backgroundColor={"#0f172a"} />
+        <Routes />
+      </AuthProvider>
+    </GestureHandlerRootView>
   );
 }
